b41676ac6861dbb579de71e2d9d660794cc9d596
yunajoe <yunajoe@yunajoeui-MacBookPro.local>
2025-08-07 22:40:34 +0900

    feat: tslint-to-eslint-config setting

--- zeppelin-web-angular/proxy.conf.js
+++ zeppelin-web-angular/proxy.conf.js
@@ -17,21 +17,21 @@ dotenv.config();
 const proxyConfig = [
   {
     context: ['/'],
-    target: 'http://localhost:8080',
+    target: 'http://localhost:8082',
     secure: false,
     changeOrigin: true
   },
   {
     context: '/ws',
-    target: 'ws://localhost:8080',
+    target: 'ws://localhost:8082',
     secure: false,
-    ws:true,
+    ws: true,
     changeOrigin: true
   }
 ];
 
 function httpUrlToWSUrl(url) {
-  return url.replace(/(http)(s)?\:\/\//, "ws$2://");
+  return url.replace(/(http)(s)?\:\/\//, 'ws$2://');
 }
 
 function setupForCorporateProxy(proxyConfig) {
@@ -44,7 +44,7 @@ function setupForCorporateProxy(proxyConfig) {
     }
     proxyConfig.forEach(function(entry) {
       if (entry.context === '/ws') {
-        entry.target = httpUrlToWSUrl(proxyServer)
+        entry.target = httpUrlToWSUrl(proxyServer);
       } else {
         entry.target = proxyServer;
       }
